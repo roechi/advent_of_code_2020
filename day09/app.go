f432e1d91e1c0a4e15c4f1572cd19eaac697a6c3
roechi <msg.remus@gmail.com>
2020-12-09 08:16:59 +0100

    adds time measurements

--- day09/app.go
+++ day09/app.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"sort"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -17,13 +18,18 @@ func main() {
 		ints[i], _ = strconv.Atoi(s)
 	}
 
+	startFaulty := time.Now()
 	faulty := FindFirstFaulty(ints, 25)
+	endFaulty := time.Since(startFaulty)
 
 	fmt.Println("First faulty: ", ints[faulty])
+	fmt.Println("Time: ", endFaulty)
 
+	startSet := time.Now()
 	set := FindContiguousSet(ints[:faulty], ints[faulty])
-
+	endSet := time.Since(startSet)
 	sort.Ints(set)
 	fmt.Println("Set: ", set)
 	fmt.Println("Solution: ", set[0]+set[len(set)-1])
+	fmt.Println("Time: ", endSet)
 }
